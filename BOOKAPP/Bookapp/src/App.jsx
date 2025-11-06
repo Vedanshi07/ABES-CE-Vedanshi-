4ea52013c431671c9e015e6e3281da6eec1068bd
vedanshi <tanupanwar200@gmail.com>
2025-11-06 15:40:52 +0530

    abc

---
+++ BOOKAPP/Bookapp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from "./components/Link1";
+
+function App() {
+  return (
+    <div>
+      <Link></Link>
+    </div>
+  )
+}
+export default App
---
+++ Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+function Card() {
+  return (
+    <div>
+        <img src="https://i.postimg.cc/3x3QzSGq/ADITIYA.jpg" alt="Dr. Aditiya"/>
+        <h1>DR. ADITIYA</h1>
+        <h3>Ph.D.</h3>
+    </div>
+  )
+}
+
+export default Card
